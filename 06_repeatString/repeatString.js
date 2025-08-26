092475b18207813cea68a03ae8db85af75b86233
Demark <demarcuslangston@gmail.com>
2025-08-26 14:31:49 -0400

    Solve exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return 'ERROR'
+    }
+    let rString = ''
+    for (let i = number; i > 0; i--) {
+        rString += string
+    }
+    return rString
 };
 
 // Do not edit below this line
