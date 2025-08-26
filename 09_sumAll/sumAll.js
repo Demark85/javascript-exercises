53576de018676ee627a13ebedd55109186c4ed40
Demark <demarcuslangston@gmail.com>
2025-08-26 19:33:10 -0400

    Solve exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return 'ERROR'
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR'
+    }
 
+    if (typeof a === 'number' && a % 1 !== 0 || typeof b === 'number' && b % 1 !== 0) {
+        return 'ERROR'
+    }
+
+    let sum = 0
+    const start = Math.min(a, b)
+    const end = Math.max(a, b)
+
+    for (let i = start; i <= end; i++) {
+        sum += i
+    }
+    return sum
+    
 };
 
 // Do not edit below this line
